6e1706946db6d0a9633d43b5a18df3e59ea7c7f9
Robert Monfera <monfera.robert@gmail.com>
2016-10-21 13:03:40 +0200

    minor cleanup of shaders: removal of unused glsl-inverse, uniform

--- lib/shaders/pick-vertex.glsl
+++ lib/shaders/pick-vertex.glsl
@@ -9,13 +9,10 @@ uniform float width;
 
 varying vec4 pickA, pickB;
 
-#pragma glslify: inverse = require("glsl-inverse")
-
 void main() {
   vec3 base = matrix * vec3(a, 1);
-  vec2 n = width *
-    normalize(screenShape.yx * vec2(d.y, -d.x)) / screenShape.xy;
-  gl_Position = vec4(base.xy/base.z + n, 0, 1);
+  vec2 n = width * normalize(screenShape.yx * vec2(d.y, -d.x)) / screenShape.xy;
+  gl_Position = vec4(base.xy / base.z + n, 0, 1);
   pickA = pick0;
   pickB = pick1;
 }
